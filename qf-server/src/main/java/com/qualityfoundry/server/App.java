71787ee22269722fb5c03fea1faad189d2484868
jonathanrt <tomko.jonathan@gmail.com>
2015-03-16 22:43:32 -0700

    Added basic project for server.

---
+++ qf-server/src/main/java/com/qualityfoundry/server/App.java
@@ -0,0 +1,13 @@
+package com.qualityfoundry.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
