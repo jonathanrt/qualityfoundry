2e9b8542b59452c69e41cd12e87753e4b2a1f851
jonathanrt <tomko.jonathan@gmail.com>
2015-03-17 00:04:58 -0700

    Added basic static content serving.

--- qf-server/src/main/java/com/qualityfoundry/server/App.java
+++ qf-server/src/main/java/com/qualityfoundry/server/App.java
@@ -1,13 +1,36 @@
 package com.qualityfoundry.server;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
 /**
- * Hello world!
- *
+ * Contains the entry point of the QF server.
+ * @author jonathanrt
  */
 public class App 
 {
-    public static void main( String[] args )
+	/**
+	 * The entry point of the QF server.
+	 * @param Command line arguments.
+	 * @throws Exception 
+	 */
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        Server jetty = new Server();
+        jetty.setStopAtShutdown(true);
+        
+        ServerConnector jettyConnector = new ServerConnector(jetty);
+        jettyConnector.setPort(8000);
+        jetty.addConnector(jettyConnector);
+        
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        context.setResourceBase(App.class.getClassLoader().getResource("web").toExternalForm());
+        context.addServlet(DefaultServlet.class, "/");
+        jetty.setHandler(context);
+        
+        jetty.start();
     }
 }
